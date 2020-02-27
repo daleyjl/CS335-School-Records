c68b1a07dc433812ba3a4cac88ae628f5a99f218
antran0325 <36683922+antran0325@users.noreply.github.com>
2020-02-27 17:47:55 -0500

    Update ScheduleView.java

--- ScheduleView.java
+++ ScheduleView.java
@@ -16,7 +16,7 @@ public class ScheduleView {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://dany.simmons.edu:3306/33501sp20_tranat?useUnicode=yes&characterEncoding=UTF-8",
+                    "jdbc:mysql://dany.simmons.edu:3306/33501sp20_tranat?characterEncoding=UTF-8",
                     user, password);
         }
         catch (Exception e){
@@ -28,12 +28,12 @@ public class ScheduleView {
         try {
             Statement selectID = connection.createStatement();
             ResultSet rs = selectID.executeQuery(
-                    "SELECT STUDENT_ID, SEMESTER, LAST_NAME, FIRST_NAME, " +
+                    "SELECT Student_ID, Semester, Last_Name, First_Name, " +
                             "COURSE_1, Start_Time_1, End_Time_1, Days_1," +
                             "COURSE_2, Start_Time_2, End_Time_2, Days_2," +
                             "COURSE_3, Start_Time_3, End_Time_3, Days_3," +
-                            "COURSE_4, Start_Time_4, End_Time_4, Days_4," + " FROM SCHEDULE," +
-                            "WHERE STUDENT_ID LIKE ID AND WHERE SEMESTER LIKE sem"
+                            "COURSE_4, Start_Time_4, End_Time_4, Days_4," + " FROM Schedule," +
+                            "WHERE Student_ID = 'ID'," + "AND WHERE Semester = 'sem'"
             );
             while (rs.next()){
                 System.out.println("Student ID: " + rs.getString(1)); //ID
