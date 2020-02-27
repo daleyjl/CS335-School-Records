64c71a3696e8158561c82344fe16ab02c88cdb0c
antran0325 <36683922+antran0325@users.noreply.github.com>
2020-02-27 14:43:17 -0500

    Update ScheduleView.java

--- ScheduleView.java
+++ ScheduleView.java
@@ -2,15 +2,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class ScheduleView {
     private Connection connection;
     ScheduleView(String user, String password){
         try{
-            Class.forName("com.mysql.jbdc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://dany.simmons.edu:3306/33501sp20_tranat?characterEncoding=UTF-8",
+                    "jdbc:mysql://dany.simmons.edu:3306/33501sp20_tranat?useUnicode=yes&characterEncoding=UTF-8",
                     user, password);
         }
         catch (Exception e){
@@ -18,12 +17,7 @@ public class ScheduleView {
         }
     }
 
-    public void listCourse (){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Insert semester: ");
-        String sem = input.next();
-        System.out.println("Insert ID: ");
-        String ID = input.next();
+    public void listCourse (String sem, String ID){
         try {
             Statement selectID = connection.createStatement();
             ResultSet rs = selectID.executeQuery(
@@ -34,7 +28,27 @@ public class ScheduleView {
                             "COURSE_4, Start_Time_4, End_Time_4, Days_4," + " FROM SCHEDULE," +
                             "WHERE STUDENT_ID LIKE ID AND WHERE SEMESTER LIKE sem"
             );
+            int course = 1;
+            while (rs.next()){
+                System.out.println("Student ID: " + rs.getString(1)); //ID
+                System.out.println("Semester: " + rs.getString(2)); //semester
+                System.out.println(rs.getString(3)
+                    + ", " + rs.getString(4)); //Student last and first name
+                System.out.println("Course 1: " + rs.getString(5) + //course 1
+                        "\t Start time: " + rs.getTime(6) +
+                        "\t End time: " + rs.getTime(7));
+                System.out.println("Course 2: " + rs.getString(8) + //course 2
+                        "\t Start time: " + rs.getTime(9) +
+                        "\t End time: " + rs.getTime(10));
+                System.out.println("Course 3: " + rs.getString(11) + //course 3
+                        "\t Start time: " + rs.getTime(12) +
+                        "\t End time: " + rs.getTime(13));
+                System.out.println("Course 4: " + rs.getString(14) + //course 4
+                        "\t Start time: " + rs.getTime(15) +
+                        "\t End time: " + rs.getTime(16));
+            }
         }
+
         catch (Exception e){
             e.printStackTrace();
         }
