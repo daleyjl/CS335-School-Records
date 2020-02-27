9d30c552b77badd4f23434d8af743bf1b7768aa9
antran0325 <36683922+antran0325@users.noreply.github.com>
2020-02-27 18:08:00 -0500

    Update ScheduleView.java

--- ScheduleView.java
+++ ScheduleView.java
@@ -27,32 +27,35 @@ public class ScheduleView {
     public void listCourse (String sem, String ID){
         try {
             Statement selectID = connection.createStatement();
+            System.out.println("Here are the courses that you applied");
             ResultSet rs = selectID.executeQuery(
                     "SELECT Student_ID, Semester, Last_Name, First_Name, " +
                             "COURSE_1, Start_Time_1, End_Time_1, Days_1," +
                             "COURSE_2, Start_Time_2, End_Time_2, Days_2," +
                             "COURSE_3, Start_Time_3, End_Time_3, Days_3," +
-                            "COURSE_4, Start_Time_4, End_Time_4, Days_4," + " FROM Schedule," +
-                            "WHERE Student_ID = 'ID'," + "AND WHERE Semester = 'sem'"
+                            "COURSE_4, Start_Time_4, End_Time_4, Days_4" +
+                            " FROM Schedule " +
+                            "WHERE Semester ='"+ sem  + "'AND Student_ID ='"+ ID +"';"
+
             );
             while (rs.next()){
-                System.out.println("Student ID: " + rs.getString(1)); //ID
-                System.out.println("Semester: " + rs.getString(2)); //semester
+                System.out.println("Student ID: " + rs.getString(1));
+                System.out.println("Semester: " + rs.getString(2));
                 System.out.println(rs.getString(3)
-                    + ", " + rs.getString(4)); //Student last and first name
-                System.out.println("Course 1: " + rs.getString(5) + //course 1
+                    + ", " + rs.getString(4));
+                System.out.println("Course 1: " + rs.getString(5) +
                         "\t Start time: " + rs.getTime(6) +
                         "\t End time: " + rs.getTime(7)+
                         "\t Days: " + rs.getString(8));
-                System.out.println("Course 2: " + rs.getString(9) + //course 2
+                System.out.println("Course 2: " + rs.getString(9) +
                         "\t Start time: " + rs.getTime(10) +
                         "\t End time: " + rs.getTime(11)+
                         "\t Days: " + rs.getString(12));
-                System.out.println("Course 3: " + rs.getString(13) + //course 3
+                System.out.println("Course 3: " + rs.getString(13) +
                         "\t Start time: " + rs.getTime(14) +
                         "\t End time: " + rs.getTime(15) +
                         "\t Days: " + rs.getString(16));
-                System.out.println("Course 4: " + rs.getString(17) + //course 4
+                System.out.println("Course 4: " + rs.getString(17) + 
                         "\t Start time: " + rs.getTime(18) +
                         "\t End time: " + rs.getTime(19) +
                         "\t Days: " + rs.getString(20));
