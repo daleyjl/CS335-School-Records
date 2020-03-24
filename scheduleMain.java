7f5c49faa9f8b3dd5d07802431f60784a31701d7
antran0325 <36683922+antran0325@users.noreply.github.com>
2020-03-24 16:27:38 -0400

    Update scheduleMain.java

--- scheduleMain.java
+++ scheduleMain.java
@@ -4,11 +4,11 @@ import java.sql.DriverManager;
 public class scheduleMain {
     public static void main (String args[]){
         Scanner input = new Scanner(System.in);
-        ScheduleView schedule = new ScheduleView("tranat", "1858423");
+        ScheduleView schedule = new ScheduleView("daleyjl", "1768443");
         System.out.println("Insert semester: ");
         String sem = input.nextLine();
         System.out.println("Insert ID: ");
-        String id = input.nextLine();
-        schedule.listCourse(sem, id);
+        int id = input.nextInt();
+        schedule.listSchedule(sem, id);
     }
 }
