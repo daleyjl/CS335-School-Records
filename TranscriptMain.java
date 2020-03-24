d6cc7a3c4589367bd85ec8d35c98adad63036154
antran0325 <36683922+antran0325@users.noreply.github.com>
2020-03-24 14:50:09 -0400

    Update TranscriptMain.java

--- TranscriptMain.java
+++ TranscriptMain.java
@@ -3,9 +3,10 @@ import java.sql.DriverManager;
 public class TranscriptMain {
     public static void main (String args[]){
         Scanner input = new Scanner(System.in);
-        TranscriptView transcript = new TranscriptView("tranat", "1858423");
+        TranscriptView transcript = new TranscriptView("daleyjl", "1768443");
         System.out.println("Insert ID: ");
         String id = input.nextLine();
         transcript.transcript(id);
+        
     }
 }
